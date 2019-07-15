c3a0bdc1e80e477edff0560f1369a36c5b4fc95f
ShirvanKhaligov <shirvanshkh@code.edu.az>
2019-07-15 21:13:26 +0400

    Mytask

---
+++ 15iyullkonsol/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15iyullkonsol
+{
+    class Program
+    {
+
+        //public static int money = 600;
+
+        //public delegate void Residual(int n);
+        //public delegate void NumbersInt(int num);
+        //public delegate void CheckPrime(int num);
+
+        //public delegate void Sum(int m);
+
+        //static void Main(string[] args)
+        //{
+        //    //ATM(601, Money);
+        //    //Balance(700);
+        //    //NumbersInt n = new NumbersInt(CheckBigNum);
+        //    Sum s = CheckPrimie;
+        //    s += Minus;
+        //    s += Double;
+        //    s(7);
+            
+
+
+            
+
+        //}
+
+        ////public static void ATM(int m, Residual Money)
+        ////{
+        ////    Money(m);
+        ////}
+
+        ////public static void Money(int n)
+        ////{
+        ////    if (n < money)
+        ////    {
+        ////        money -= n;
+
+        ////        Console.WriteLine($"Your money: {money} ");
+        ////    }
+        ////    else
+        ////    {
+        ////        Console.WriteLine("Money doesn't have");
+        ////    }
+        ////}
+        ////public static void Balance(int f)
+        ////{
+        ////    if (f > money)
+        ////    {
+        ////        Console.WriteLine("Money doesn't have.Your Balance is "+money+"azn");
+        ////    }
+        ////    else
+        ////    {
+
+        ////        Console.WriteLine("ok");
+        ////    }
+
+
+
+        ////}
+        
+        //static void CheckPrimie(int k)
+        //{
+        //    int count = 0;
+        //    for (int i = 2; i <= k; i++)
+        //    {
+
+        //        if (k % i == 0)
+        //        {
+        //            count++;
+        //        }
+
+        //    }
+
+
+        //    if (count > 2)
+        //    {
+        //        Console.WriteLine("This is not Prime");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("This is Prime");
+        //    }
+
+
+
+        //}
+        //static void Minus(int m)
+        //{
+        //    if (m > 0)
+        //    {
+        //        Console.WriteLine("Musbetdir");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Menfidir ala ");
+        //    }
+        //}
+        //static void Double(int h)
+        //{
+        //    if (h % 2 == 0)
+        //    {
+        //        Console.WriteLine("Müsbetdir");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Menfidir");
+        //    }
+        //}
+
+    }
+    
+}
